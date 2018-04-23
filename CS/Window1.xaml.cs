8c70f9984196c2687a26b7e6d49a0bd6746fb8b3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Windows;
+
+namespace WpfApplication1 {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+
+    public static class NWindData {
+        static nwindDataSet ds;
+        static NWindData() {
+            ds = new nwindDataSet();
+            nwindDataSetTableAdapters.CategoriesTableAdapter categoriesAdapter = 
+                new nwindDataSetTableAdapters.CategoriesTableAdapter();
+            nwindDataSetTableAdapters.ProductsTableAdapter productsAdapter = 
+                new nwindDataSetTableAdapters.ProductsTableAdapter();
+            categoriesAdapter.Fill(ds.Categories);
+            productsAdapter.Fill(ds.Products);
+        }
+
+        public static DataView Products { get { return ds.Products.DefaultView; } }
+        public static DataView Categories { get { return ds.Categories.DefaultView; } }
+    }
+}
